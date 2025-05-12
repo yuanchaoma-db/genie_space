bb6a605c6c4cb16577d631de677e38dc8ad35405
Yuanchao Ma <yuanchao.ma@databricks.com>
2025-05-12 12:14:33 -0700

    add logging

--- genie_space/app.py
+++ genie_space/app.py
@@ -8,8 +8,13 @@ import os
 from dotenv import load_dotenv
 import sqlparse
 from flask import request
+import logging
 
 load_dotenv()
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 # Create Dash app
 app = dash.Dash(
@@ -384,6 +389,7 @@ def handle_all_inputs(s1_clicks, s2_clicks, s3_clicks, s4_clicks, send_clicks, s
     prevent_initial_call=True
 )
 def get_model_response(trigger_data, current_messages, chat_history):
+    logger.info(f"get_model_response called with trigger_data={trigger_data}, current_messages={current_messages}, chat_history={chat_history}")
     if not trigger_data or not trigger_data.get("trigger"):
         return dash.no_update, dash.no_update, dash.no_update, dash.no_update
     
